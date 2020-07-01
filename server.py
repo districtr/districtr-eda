43a60a804569be636ce7db06c1e8ce3ce920397a
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-07-01 21:12:05 +0100

    Fix bug when we don't have a shapefile

--- server.py
+++ server.py
@@ -37,39 +37,33 @@ def plan_metrics():
             state_shapefile_path = state_shapefile_paths[state]
             state_graph = gerrychain.Graph.from_file(state_shapefile_path)
             state_graph.to_json(f'./dual_graphs/{state}_dual.json')
+            # OK, so now we are guaranteed to have the state graph.
+            # Form the partition with the JSON path (requires state graph)
+            # partition = gerrychain.Partition.from_districtr_file(state_graph, JSON_PATH, 
+            #                                                     updaters=None)
+
+            # This is taken from the Districtr function from_districtr_file
+            # https://gerrychain.readthedocs.io/en/latest/_modules/gerrychain/partition/partition.html
+            id_column_key = plan["idColumn"]["key"]
+            districtr_assignment = plan["assignment"]
+            try:
+                node_to_id = {node: str(state_graph.nodes[node][id_column_key]) for node in state_graph}
+            except KeyError:
+                raise TypeError(
+                    "The provided graph is missing the {} column, which is "
+                    "needed to match the Districtr assignment to the nodes of the graph."
+                )
+            assignment = {node: districtr_assignment[node_to_id[node]] for node in state_graph}
+            partition = gerrychain.Partition(state_graph, assignment, None)
+
+            # Now that we have the partition, calculate all the different metrics
+            cut_edges = (partition['cut_edges'])
+            contiguity = (gerrychain.constraints.contiguity.contiguous(partition))
+
+
+            response = flask.jsonify({'cut_edges': str(cut_edges), 'contiguity': contiguity})
+            #response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
         except ValueError:
-            return ("Don't have either dual graph or shapefile for this state")
-    # OK, so now we are guaranteed to have the state graph.
-    # Form the partition with the JSON path (requires state graph)
-    # partition = gerrychain.Partition.from_districtr_file(state_graph, JSON_PATH, 
-    #                                                     updaters=None)
-
-    # This is taken from the Districtr function from_districtr_file
-    # https://gerrychain.readthedocs.io/en/latest/_modules/gerrychain/partition/partition.html
-    id_column_key = plan["idColumn"]["key"]
-    districtr_assignment = plan["assignment"]
-    try:
-        node_to_id = {node: str(state_graph.nodes[node][id_column_key]) for node in state_graph}
-    except KeyError:
-        raise TypeError(
-            "The provided graph is missing the {} column, which is "
-            "needed to match the Districtr assignment to the nodes of the graph."
-        )
-    assignment = {node: districtr_assignment[node_to_id[node]] for node in state_graph}
-    partition = gerrychain.Partition(state_graph, assignment, None)
-
-    # Now that we have the partition, calculate all the different metrics
-    cut_edges = (partition['cut_edges'])
-    contiguity = (gerrychain.constraints.contiguity.contiguous(partition))
-
-
-    response = flask.jsonify({'cut_edges': str(cut_edges), 'contiguity': contiguity})
-    #response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-    '''
-
-    response = flask.jsonify({'message': 'hello'})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-    '''
-
+            response = flask.jsonify({'error': "Don't have either dual graph or shapefile for this state"})
+            return response
--- test_request.js
+++ test_request.js
@@ -394,11 +394,14 @@ function test_request() {
 
   const test = JSON.stringify(test_json);
   console.log(test);
-  fetch("http://127.0.0.1:5000/", {
+  //fetch("http://127.0.0.1:5000", {
+  fetch("http://lieu.pythonanywhere.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      //"Content-Type": "text/plain",
     },
+    mode: "cors",
     body: JSON.stringify(test_json),
   })
     .then((res) => res.json())
