4b062e826e83c6e74c3a6fd8b4357dddd93a9749
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-07-01 10:44:46 +0100

    Add server.py with requirements.txt

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask import request
-import sys
 import json
 import geopandas as gpd
 import gerrychain
-import networkx as nx
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -33,15 +30,27 @@ def plan_metrics():
             state_graph = gerrychain.Graph.from_file(state_shapefile_path)
             state_graph.to_json(f'./dual_graphs/{state}_dual.json')
         except ValueError:
-            return("Don't have either dual graph or shapefile for this state")
+            return ("Don't have either dual graph or shapefile for this state")
     # OK, so now we are guaranteed to have the state graph.
-    # Form the partition with the JSON path (requires state graph
-    partition = gerrychain.Partition.from_districtr_file(state_graph, JSON_PATH, 
-                                                         updaters=None)
+    # Form the partition with the JSON path (requires state graph)
+    # partition = gerrychain.Partition.from_districtr_file(state_graph, JSON_PATH, 
+    #                                                     updaters=None)
+
+    # This is taken from the Districtr function from_districtr_file
+    # https://gerrychain.readthedocs.io/en/latest/_modules/gerrychain/partition/partition.html
+    id_column_key = plan["idColumn"]["key"]
+    districtr_assignment = plan["assignment"]
+    try:
+        node_to_id = {node: str(state_graph.nodes[node][id_column_key]) for node in state_graph}
+    except KeyError:
+        raise TypeError(
+            "The provided graph is missing the {} column, which is "
+            "needed to match the Districtr assignment to the nodes of the graph."
+        )
+    assignment = {node: districtr_assignment[node_to_id[node]] for node in state_graph}
+    partition = (state_graph, assignment, None)
+
     # Now that we have the partition, calculate all the different metrics
-    print(partition['cut_edges'])
-    print(gerrychain.constraints.contiguity.contiguous(partition))
-
-
-
-
+    cut_edges = (partition['cut_edges'])
+    contiguity = (gerrychain.constraints.contiguity.contiguous(partition))
+    return ({'cut_edges': cut_edges, 'contiguity': contiguity})
