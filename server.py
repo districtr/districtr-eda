b096d77bb7d05f5c0a794a28a73c87c65e93cb58
Nick Doiron <ndoiron@mapmeld.com>
2020-09-14 09:50:30 -0400

    Export photo of supported plans

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import os.path
+import base64
+import io
+import time
+
 import flask
 from flask import Flask
 from flask import request
@@ -7,7 +11,7 @@ import json
 import geopandas as gpd
 import gerrychain
 import networkx as nx
-import time
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -18,13 +22,14 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 state_shapefile_paths = {
     "iowa": f'{dir_path}/shapefiles/IA_counties/IA_counties.shp',
     "texas": f'{dir_path}/shapefiles/TX_vtds/TX_vtds.shp',
+    "forsyth_nc": f'{dir_path}/shapefiles/forsyth_nc/forsyth-nc.shp',
 }
 
 def form_assignment_from_state_graph(districtr_assignment, node_to_id, state_graph):
     '''
     Converts a Districtr graph assignment (which node belongs to which district)
     into an assignment that works with Gerrychain
-    Requires a Districtr graph assignment, 
+    Requires a Districtr graph assignment,
     a mapping of Districtr nodes to Gerrychain nodes,
     and the appropriate state dual graph.
     Assigns all unassigned nodes to a district -1.
@@ -46,11 +51,125 @@ def form_assignment_from_state_graph(districtr_assignment, node_to_id, state_gra
 
 cached_gerrychain_graphs = {}
 
+@app.route('/picture', methods=['POST'])
+def plan_pic():
+    plan = request.get_json()
+
+    state = plan['placeId'] # get the plan id of the Districtr plan
+
+    if state not in state_shapefile_paths:
+        return 'no shapefile available'
+
+    # Check if we already have a dual graph of the state
+    dual_graph_path = f"{dir_path}/dual_graphs/mggg-dual-graphs/{state}.json"
+    print(dual_graph_path)
+
+    if state in cached_gerrychain_graphs:
+        start = time.time()
+        print("Retrieving the state graph from memory..")
+        state_graph = cached_gerrychain_graphs[state]
+        end = time.time()
+        print(f"Time taken to retrieve the state graph from memory: {end-start}")
+    elif os.path.isfile(dual_graph_path):
+        # TODO timeit this --- how long does it take to load into memory?
+        # this takes the vast majority of the time
+        start = time.time()
+        state_graph = gerrychain.Graph.from_json(dual_graph_path)
+        print("Caching the state graph...")
+        cached_gerrychain_graphs[state] = state_graph
+        end = time.time()
+        print(f"Time taken to load into gerrychain Graph from json: {end-start}")
+    else:
+        response = flask.jsonify({'error': "Don't have dual graph for this state"})
+        return response
+
+    id_column_key = plan["idColumn"]["key"]
+    districtr_assignment = plan["assignment"]
+
+    try:
+        node_to_id = {node: str(state_graph.nodes[node][id_column_key]) for node in state_graph}
+    except KeyError:
+        response = flask.jsonify({'error':
+            "The provided graph is missing the {} column, which is "
+            "needed to match the Districtr assignment to the nodes of the graph."
+        })
+        return response
+
+    # If everything checks out, form a Partition
+    # TODO timeit this --- how long does it take?
+    start_1 = time.time()
+    assignment = form_assignment_from_state_graph(districtr_assignment, node_to_id, state_graph)
+    end_1 = time.time()
+    print(f"Time taken to form assignment from dual graph: {end_1-start_1}")
+
+    # TODO timeit this --- how long does it take?
+    start_2 = time.time()
+    partition = gerrychain.Partition(state_graph, assignment, None)
+    end_2 = time.time()
+    print(f"Time taken to form partition from assignment: {end_2 - start_2}")
+
+    geometries = gpd.read_file(state_shapefile_paths[state])
+    parts_in_partition = gerrychain.constraints.contiguity.affected_parts(partition)
+    cutoff_blank = 1
+    if -1 in parts_in_partition:
+        cutoff_blank = 0
+
+    cs_bright = ["#555555", "#0099cd",
+    "#ffca5d",
+    "#00cd99",
+    "#99cd00",
+    "#cd0099",
+    "#9900cd",
+    "#8dd3c7",
+    "#bebada",
+    "#fb8072",
+    "#80b1d3",
+    "#fdb462",
+    "#b3de69",
+    "#fccde5",
+    "#bc80bd",
+    "#ccebc5",
+    "#ffed6f",
+    "#ffffb3",
+    "#a6cee3",
+    "#1f78b4",
+    "#b2df8a",
+    "#33a02c",
+    "#fb9a99",
+    "#e31a1c",
+    "#fdbf6f",
+    "#ff7f00",
+    "#cab2d6",
+    "#6a3d9a",
+    "#b15928",
+    "#64ffda",
+    "#00B8D4",
+    "#A1887F",
+    "#76FF03",
+    "#DCE775",
+    "#B388FF",
+    "#FF80AB",
+    "#D81B60",
+    "#26A69A",
+    "#FFEA00",
+    "#6200EA"][cutoff_blank:len(parts_in_partition) + cutoff_blank]
+
+    axesplot = partition.plot(geometries, cmap=plt.cm.colors.ListedColormap(cs_bright))
+    axesplot.axis("off")
+
+    pic_IObytes = io.BytesIO()
+    axesplot.figure.savefig(pic_IObytes, format='png')
+    pic_IObytes.seek(0)
+    pic_hash = base64.b64encode(pic_IObytes.read())
+
+    return str(pic_hash)
+
+
 @app.route('/', methods=['POST'])
 # Takes a Districtr JSON and returns whether or not it's contiguous and number of cut edges.
 def plan_metrics():
     plan = request.get_json()
-    
+
     state = plan['placeId'] # get the state of the Districtr plan
     # Check if we already have a dual graph of the state
     dual_graph_path = f"{dir_path}/dual_graphs/mggg-dual-graphs/{state}.json"
